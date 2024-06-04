640e103c3a7efd59af1d1b18359440d760c522ef
DspStudent <devisriprasad948@gmail.com>
2024-06-04 16:54:29 +0530

    first

---
+++ CoverInWater.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class CoverInWater{
+	public static int f(String s){
+		String str[]=s.split("#");
+		// System.out.println(Arrays.toString(str));
+		int ans=0;
+		for(String s1:str){
+			if(s1.length()>2)return 2;
+			ans+=s1.length();
+		}
+		return ans;
+	}
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+			int n=sc.nextInt();
+			String q=sc.next();
+			System.out.println(f(q));
+		}
+	}
+}
